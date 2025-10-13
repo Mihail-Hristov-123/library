8a87222015a2ac9967ab4f5473c4097a7a3fd132
Mihail-Hristov-123 <mihail.hristov@intellias.com>
2025-10-13 10:18:31 +0300

    chore: set up ts and dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("works");
+import Koa from "koa";
+import { configDotenv } from "dotenv";
+configDotenv();
+const app = new Koa();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening to port: ${PORT}`);
+});
