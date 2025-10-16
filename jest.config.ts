a6a597436e6b80c53e6d71eafb65de2bc869f4c3
Mihail-Hristov-123 <mihail.hristov@intellias.com>
2025-10-16 16:12:42 +0300

    chore: set up testing with jest

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  transform: {
+    "^.+\\.ts$": ["ts-jest", { useESM: true }],
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts"],
+};
+
+export default config;
---
+++ tests/utils/handleMissingParam.test.ts
@@ -0,0 +1,18 @@
+import { CustomError } from "../../src/CustomError.js";
+import { handleMissingParam } from "../../src/utils/handleMissingParam.js";
+
+describe("handleMissingParam", () => {
+  it("should throw when param is undefined", () => {
+    expect(() => handleMissingParam(undefined)).toThrow(CustomError);
+  });
+  it("should throw when param is an empty string", () => {
+    expect(() => handleMissingParam("")).toThrow(CustomError);
+  });
+  it("should throw when param is a blank string", () => {
+    expect(() => handleMissingParam("  ")).toThrow(CustomError);
+  });
+
+  it("should not throw when param is valid - non empty string", () => {
+    expect(() => handleMissingParam("1")).not.toThrow();
+  });
+});
